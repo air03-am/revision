2e8aa8fb08f994e68547e17eecdf45603365d724
Maria Gonzalez <airamjgr@gmail.com>
2025-05-26 22:38:43 -0600

    feat:Corrección de errores y estructura del código

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,22 +1,35 @@
 const baseEndpoint = 'https://api.github.com';
 const usersEndpoint = `${baseEndpoint}/users`;
-const $n = document.querySelector('name');
-const $b = document.querySelector('#blog');
-const $l = document.querySelector('.location');
+// Cambie $n, $b, $l por $name, $blog, $location para mayor claridad
+// Cambia name a .name (selector e clase)
+// Cambia #blog a .blog (selector e clase)
 
-function displayUser(username) {
-  $n.textContent = 'cargando...';
-  const response = await fetch(`${usersEndpoint}/${username}`);
-  console.log(data);
-  $n.textContent = '${data.name}';
-  $b.textContent = '${data.blog}';
-  $l.textContent = '${data.location}';
+const $name = document.querySelector('.name');
+const $blog = document.querySelector('.blog');
+const $location = document.querySelector('.location');
+
+
+async function displayUser(username) { //Añadi async
+    $name.textContent = 'cargando...';
+    $blog.textContent = '';
+    $location.textContent = '';// limpia los campos antes de cargar nuevos datos
+  try { //Se inicializa un bloque try para manejar errores
+    const response = await fetch(`${usersEndpoint}/${username}`);
+    // Convierte la respuesta a JSON
+    const data = await response.json();
+
+    //Mostrar los datos en la página
+    console.log(data); // Muestra los datos en la consola
+    $name.textContent = `${data.name}`; //Se cambiaron las comillas
+    $blog.textContent = `${data.blog}`;
+    $location.textContent = `${data.location}`;
+  } catch (error) { // Manejo de errores
+    handleError(err);
+  }
 }
-
-function handleError(err) {
-  console.log('OH NO!');
+  function handleError(err) {
+  console.log('OH NO!'); // Muestra un mensaje de error en la consola
   console.log(err);
-  n.textContent = `Algo salió mal: ${err}`
+  $name.textContent = `Algo salió mal: ${err}`
 }
-
-displayUser('stolinski').catch(handleError);
+displayUser('stolinski'); // Se elimino el catch para evitar ser redundante
